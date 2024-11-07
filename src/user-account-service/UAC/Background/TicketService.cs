432eb530a07299cc2a818060ac9a2b1186e0f2d6
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-11-07 22:28:10 +0100

    Created UAC service in dotnet

---
+++ src/user-account-service/UAC/Background/TicketService.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace UAC.Background
+{
+    public class TicketService(ILogger<TicketService> logger) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken st)
+        {
+            var i = 0;
+            while (!st.IsCancellationRequested)
+            {
+                logger.LogInformation("Running ticket service running {TickerRunCount} times", i++);
+                await Task.Delay(1000, st);
+            }
+        }
+    }
+}
---
+++ src/user-account-service/UAC/Program.cs
@@ -0,0 +1,12 @@
+﻿using UAC.Background;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<TicketService>();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapGet("/", () => "Hello World!");
+app.Run();
